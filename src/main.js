aca1bbb44729002d2da7dc5ba8eaae924a83db10
SimJH99 <s99s4481@gmail.com>
2024-02-06 12:14:54 +0900

    bootstrap import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js';
+import '@/assets/css/bootstrap.min.css'
 
 // index.html의 id인 app에 마운트가 되도록 하는 코드
 // createApp(App).mount('#app')
