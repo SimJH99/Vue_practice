e184e5a00dae3e5b24d9ae144d60a8b7cf3585ac
SimJH99 <s99s4481@gmail.com>
2024-02-06 09:48:53 +0900

    script basic

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,14 +1,17 @@
 <template>
-
-    <h1>hello world</h1>
+    <h1>hello world {{ myLang }}</h1>
+    <input type="text" value="myvalue">
 </template>
   
 <script>
-
 export default {
     data(){
-        
+        return{
+            myLang: "python"
+        }
     }
+
+    // axios를 써서 서버에 data요청 => result => mylang = result.lang;
 };
 
 </script>
