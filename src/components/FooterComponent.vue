8aeb31355ff2b60019f9b41997f9b32f6eaad611
SimJH99 <s99s4481@gmail.com>
2024-02-06 19:26:57 +0900

    header, footer design

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        Footer
+    <div class="content-container" style="margin-bottom: 100px;">
+        
     </div>
+    <footer class="fixed-bottom bg-light text-center p-3">
+        @ 2024 copyrights: <a class="text-dark" href="#">hello@naver.com</a>
+    </footer>
 </template>
 
 <script>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="navbar-collapse w-100 order-1 order-md-0 dual-collapse2">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto" v-if="userRole==='ADMIN'">
                 <li class="nav-item" ><a class="nav-link" href="/post/list">회원관리</a></li>
                 <li class="nav-item"><a class="nav-link" href="/author/create">상품관리</a></li>
                 <li class="nav-item"><a class="nav-link" href="/author/create">주문관리</a></li>
@@ -12,13 +12,23 @@
         </div>
         <div class="navbar-collapse w-100 order-3 dual-collapse2">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item" ><a class="nav-link" href="/post/list">상품목록</a></li>
-                <li class="nav-item"><a class="nav-link" href="/author/create">회원가입</a></li>
-                <li class="nav-item">
-                    <a class="nav-link"  href="/author/login-page">로그인</a>
+                <li class="nav-item" >
+                    <a class="nav-link" href="/items/list">상품목록</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/doLogout">로그아웃</a>
+                <li class="nav-item" v-if="!isLogin">
+                    <a class="nav-link" href="/ordercart">장바구니</a>
+                </li>
+                <li class="nav-item" v-if="!isLogin">
+                    <a class="nav-link" href="/mypage">MyPage</a>
+                </li>
+                <li class="nav-item" v-if="!isLogin">
+                    <a class="nav-link" href="/member/create">회원가입</a>
+                </li>
+                <li class="nav-item" v-if="!isLogin">
+                    <a class="nav-link" href="/login">로그인</a>
+                </li>
+                <li class="nav-item" v-if="isLogin">
+                    <a class="nav-link" href="#" @click="doLogout">로그아웃</a>
                 </li>
             </ul>
         </div>
@@ -26,10 +36,26 @@
 </template>
 <script>
 export default {
-    setup () {
-        return {}
+    data(){
+        return {
+            isLogin: false, 
+            userRole: null
+        }
+    },
+    created() {
+        if(localStorage.getItem("token")){
+            this.isLogin = true;
+            this.userRole = localStorage.getItem("role");
+        }
+    },
+    methods: {
+        doLogout(){
+            localStorage.clear();
+            // localStorage.removeItem("token");
+            // localStorage.removeItem("Role");
+            // localStorage.removeItem("email");
+            window.location.reload();
+        }
     }
 }
 </script>
-<style lang="scss" scoped>
-</style>
